a54ac2e8b771e55a351f04fcce9c0801855c4693
Clayton Rieck <cjrieck123@gmail.com>
2014-10-25 15:29:00 -0400

    added base project files

---
+++ App/iOS Starter Kit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS Starter Kit
+//
+//  Created by Clayton Rieck on 10/25/14.
+//  Copyright (c) 2014 SK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ App/iOS Starter Kit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOS Starter Kit
+//
+//  Created by Clayton Rieck on 10/25/14.
+//  Copyright (c) 2014 SK. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
